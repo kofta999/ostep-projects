ad33612d7e3be93246f4cdbddd8d4c495d0881fc
kofta999 <mostafaxxx555@gmail.com>
2026-01-06 16:44:34 +0200

    refactor(tail): use anyhow more and improve args parsing

--- file-api/tail/src/main.rs
+++ file-api/tail/src/main.rs
@@ -8,65 +8,57 @@ const DEFAULT_COUNT: i32 = 10;
 const BUFFER_SIZE: u16 = 4096;
 
 fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        anyhow::bail!("Usage: tail -n <file>");
-    }
-    let mut target_count: Option<i32> = None;
+    let args = std::env::args().skip(1);
 
-    if args[1].starts_with("-") {
-        let temp = args[1][1..].parse::<i32>().context("n must be a number")?;
+    let mut target_count = DEFAULT_COUNT;
+    let mut filename = String::new();
 
-        target_count = Some(temp);
-
-        if args.len() != 3 {
-            anyhow::bail!("<file> required");
+    for arg in args {
+        if let Some(count) = arg.strip_prefix('-') {
+            target_count = count.parse::<i32>().context("n must be a number")?;
+        } else {
+            filename = arg;
         }
     }
 
-    let filename = if target_count.is_some() {
-        &args[2]
-    } else {
-        &args[1]
+    if filename.is_empty() {
+        anyhow::bail!("Usage: tail -n <file>");
     }
-    .as_str();
 
-    let mut target_count = target_count.unwrap_or(DEFAULT_COUNT);
+    let fd = open(filename.as_str(), OFlag::O_RDONLY, Mode::S_IRUSR)?;
 
-    if let Ok(fd) = open(filename, OFlag::O_RDONLY, Mode::S_IRUSR) {
-        let file_size = lseek(&fd, 0, Whence::SeekEnd)?;
-        let mut pos = file_size;
-        let mut buf = [0u8; BUFFER_SIZE as usize];
+    let file_size = lseek(&fd, 0, Whence::SeekEnd)?;
+    let mut pos = file_size;
+    let mut buf = [0u8; BUFFER_SIZE as usize];
 
-        while target_count >= 0 && pos > 0 {
-            let to_read = std::cmp::min(pos, BUFFER_SIZE as i64);
-            pos -= to_read;
+    while target_count >= 0 && pos > 0 {
+        let to_read = std::cmp::min(pos, BUFFER_SIZE as i64);
+        pos -= to_read;
 
-            lseek(&fd, pos, Whence::SeekSet)?;
-            read(&fd, &mut buf[..to_read as usize])?;
+        lseek(&fd, pos, Whence::SeekSet)?;
+        read(&fd, &mut buf[..to_read as usize])?;
 
-            for (i, &byte) in buf[..to_read as usize].iter().enumerate().rev() {
-                if byte == b'\n' {
-                    target_count -= 1;
-                    if target_count < 0 {
-                        pos += i as i64 + 1;
-                        break;
-                    }
+        for (i, &byte) in buf[..to_read as usize].iter().enumerate().rev() {
+            if byte == b'\n' {
+                target_count -= 1;
+                if target_count < 0 {
+                    pos += i as i64 + 1;
+                    break;
                 }
             }
         }
-
-        lseek(&fd, pos, Whence::SeekSet);
-        loop {
-            let bytes_read = read(&fd, &mut buf).unwrap();
-            if bytes_read == 0 {
-                break;
-            }
-            io::stdout().write_all(&buf[..bytes_read]);
-        }
-
-        close(fd)?;
     }
+
+    lseek(&fd, pos, Whence::SeekSet)?;
+    loop {
+        let bytes_read = read(&fd, &mut buf).unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        io::stdout().write_all(&buf[..bytes_read])?;
+    }
+
+    close(fd)?;
 
     Ok(())
 }
