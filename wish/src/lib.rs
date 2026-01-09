faa9c94e3b581cd1520181d96f930dc6d1062822
kofta999 <mostafaxxx555@gmail.com>
2026-01-10 00:32:10 +0200

    feat(wish): add command execution

--- wish/src/lib.rs
+++ wish/src/lib.rs
@@ -1,8 +1,18 @@
 use std::{
+    ffi::CString,
     io::{self, Write},
     process::exit,
 };
+
+use anyhow::{Result, bail};
+use nix::{
+    sys::wait::waitpid,
+    unistd::{AccessFlags, ForkResult, access, execv, fork},
+};
+
 const PS1: &str = "wish> ";
+const GLOBAL_ERR_MSG: &str = "An error has occurred";
+const PATH: [&str; 1] = ["/bin"];
 
 pub fn run() {
     let stdin = io::stdin();
@@ -29,7 +39,11 @@ pub fn run() {
         // eval
         match command {
             "exit" => handle_exit(),
-            _ => process_command(command),
+            _ => {
+                if process_command(command).is_err() {
+                    eprintln!("{GLOBAL_ERR_MSG}");
+                }
+            }
         }
 
         // print
@@ -40,6 +54,38 @@ fn handle_exit() {
     exit(0);
 }
 
-fn process_command(command: &str) {
-    eprintln!("An error has occurred");
+fn process_command(command: &str) -> Result<()> {
+    let args = parse_args(command);
+
+    if args.is_empty() {
+        bail!("{GLOBAL_ERR_MSG}")
+    }
+
+    for path in PATH {
+        let full_path = format!("{path}/{}", args[0]);
+
+        if access(full_path.as_str(), AccessFlags::F_OK).is_ok() {
+            match unsafe { fork() } {
+                Ok(ForkResult::Parent { child }) => {
+                    waitpid(child, None)?;
+                    return Ok(());
+                }
+                Ok(ForkResult::Child) => {
+                    let c_args: Vec<CString> = args
+                        .iter()
+                        .map(|arg| CString::new(*arg).expect("can't make cstring"))
+                        .collect();
+
+                    execv(&CString::new(full_path)?, &c_args)?;
+                }
+                Err(_) => bail!("{GLOBAL_ERR_MSG}"),
+            }
+        };
+    }
+
+    bail!("{GLOBAL_ERR_MSG}")
+}
+
+fn parse_args(command: &str) -> Vec<&str> {
+    command.split_whitespace().collect()
 }
